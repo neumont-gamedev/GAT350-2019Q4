966e816187a5c040a9b13489010fe253118ffc73
Neumont College - Game Development <31079247+neumont-gamedev@users.noreply.github.com>
2019-10-01 16:16:53 -0600

    vbo/vao triangle

--- gat350/main.cpp
+++ gat350/main.cpp
@@ -1,5 +1,31 @@
-#include <glad\glad.h>
 #include "sdl.h"
+#include <glad\glad.h>
+
+const GLfloat positions[] =
+{
+	-0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f
+};
+
+const GLfloat colors[] =
+{
+	1.0f, 0.0f, 0.0f,
+	0.0f, 1.0f, 0.0f,
+	0.0f, 0.0f, 1.0f
+};
+
+const GLfloat vertices[] =
+{
+	-0.5f, -0.5f, 0.0f,	1.0f, 0.0f, 0.0f,
+	 0.0f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+	 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+};
+
+const GLuint indices[] = {
+	0, 1, 2,
+	2, 3, 0
+};
 
 int main(int argc, char** argv)
 {
@@ -15,6 +41,10 @@ int main(int argc, char** argv)
 		SDL_Log("Failed to create window: %s", SDL_GetError());
 	}
 
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
 	SDL_GL_SetSwapInterval(1);
@@ -23,6 +53,84 @@ int main(int argc, char** argv)
 	if (!gladLoadGL()) {
 		exit(-1);
 	}
+
+	//
+	GLuint position_vbo;
+	glGenBuffers(1, &position_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, position_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+	
+	GLuint color_vbo;
+	glGenBuffers(1, &color_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, color_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+	GLuint vertex_vbo;
+	glGenBuffers(1, &vertex_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	//glEnableVertexAttribArray(0);
+	//glBindBuffer(GL_ARRAY_BUFFER, position_vbo);
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte*)NULL);
+
+	//glEnableVertexAttribArray(1);
+	//glBindBuffer(GL_ARRAY_BUFFER, color_vbo);
+	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte*)NULL);
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLubyte*)NULL);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLubyte*)(3 * sizeof(GLfloat)));
+
+	const char* vertex_source = "#version 430 \n \
+	in vec3 vposition; \n \
+	in vec3 vcolor; \n \
+	out vec3 fcolor; \n \
+	void main() \n \
+	{ \n \
+		fcolor = vcolor; \n \
+		gl_Position = vec4(vposition, 1.0); \n \
+	}";
+
+	GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_source, 0);
+	glCompileShader(vertex_shader);
+	GLint status = 0;
+	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &status);
+	if (status == GL_FALSE)
+	{
+		exit(-1);
+	}
+
+	const char* fragment_source = "#version 430 \n \
+	in vec3 fcolor; \n \
+	out vec4 color; \n \
+	void main() \n \
+	{ \n \
+		color = vec4(fcolor, 1.0); \n \
+	}";
+
+	GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_source, 0);
+	glCompileShader(fragment_shader);
+	status = 0;
+	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &status);
+	if (status == GL_FALSE)
+	{
+		exit(-1);
+	}
+
+	GLuint shader_program = glCreateProgram();
+	glAttachShader(shader_program, vertex_shader);
+	glAttachShader(shader_program, fragment_shader);
+	glLinkProgram(shader_program);
+	glUseProgram(shader_program);
 
 	bool quit = false;
 	while (!quit)
@@ -47,19 +155,15 @@ int main(int argc, char** argv)
 		glClearColor(0.85f, 0.85f, 0.85f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex2f(-0.5f, -0.5f);
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex2f(0.0f, 0.5f);
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex2f(0.5f, -0.5f);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glEnd();
 
 		SDL_GL_SwapWindow(window);
 	}
+
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
 
 	return 0;
 }
