6194602daef061ae2959098a04b89e6663c6d616
Neumont College - Game Development <31079247+neumont-gamedev@users.noreply.github.com>
2019-10-09 09:52:29 -0600

    depth test and culling

--- gat350/engine/renderer/renderer.cpp
+++ gat350/engine/renderer/renderer.cpp
@@ -18,12 +18,20 @@ bool Renderer::Initialize(u32 width, u32 height, bool fullscreen)
 
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+	
 	SDL_GL_SetSwapInterval(1);
 
 	m_context = SDL_GL_CreateContext(m_window);
 	if (!gladLoadGL()) {
 		return false;
 	}
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
+	glEnable(GL_CULL_FACE);
+	glFrontFace(GL_CCW);
+	glCullFace(GL_BACK);
 
 	return true;
 }
@@ -37,7 +45,7 @@ void Renderer::Shutdown()
 void Renderer::ClearBuffer()
 {
 	glClearColor(0.85f, 0.85f, 0.85f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 void Renderer::SwapBuffer()
--- gat350/main.cpp
+++ gat350/main.cpp
@@ -15,6 +15,51 @@ const GLuint indices[] = {
 	2, 3, 0
 };
 
+const GLfloat cube_vertices[] = {
+	// front
+	-1.0, -1.0,  1.0, 1.0, 0.0, 0.0,
+	 1.0, -1.0,  1.0, 0.0, 1.0, 0.0,
+	 1.0,  1.0,  1.0, 0.0, 0.0, 1.0,
+	-1.0,  1.0,  1.0, 1.0, 1.0, 1.0,
+	// back
+	-1.0, -1.0, -1.0, 1.0, 0.0, 0.0,
+	 1.0, -1.0, -1.0, 0.0, 1.0, 0.0,
+	 1.0,  1.0, -1.0, 0.0, 0.0, 1.0,
+	-1.0,  1.0, -1.0, 1.0, 1.0, 1.0
+};
+
+GLushort cube_elements[] = {
+	// front
+	0, 1, 2,
+	2, 3, 0,
+	// right
+	1, 5, 6,
+	6, 2, 1,
+	// back
+	7, 6, 5,
+	5, 4, 7,
+	// left
+	4, 0, 3,
+	3, 7, 4,
+	// bottom
+	4, 5, 1,
+	1, 0, 4,
+	// top
+	3, 2, 6,
+	6, 7, 3
+};
+
+struct Transform
+{
+	glm::vec3 translation;
+	glm::mat4 matrix;
+
+	void Update()
+	{
+		matrix = glm::translate(glm::mat4(1.0f), translation);
+	}
+};
+
 int main(int argc, char** argv)
 {
 	filesystem::set_current_path("content");
@@ -34,12 +79,12 @@ int main(int argc, char** argv)
 	GLuint vbo;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
 
 	GLuint ebo;
 	glGenBuffers(1, &ebo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_elements), cube_elements, GL_STATIC_DRAW);
 
 	GLuint vao;
 	glGenVertexArrays(1, &vao);
@@ -59,7 +104,20 @@ int main(int argc, char** argv)
 	program.Link();
 	program.Use();
 
-	glm::mat4 mx = glm::mat4(1.0f);
+	//glm::mat4 mxProjection = glm::ortho(0.0f, 800.0f, 600.0f, 0.0f);
+	glm::mat4 mxProjection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.01f, 100.0f);
+	glm::vec3 eye = glm::vec3(0.0f, 0.0f, 5.0f);
+	glm::mat4 mxView = glm::lookAt(eye, glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	
+	Transform transform;
+	transform.translation = glm::vec3(0.0f, 1.0f, 0.0f);
+
+	glm::mat4 mxModel = glm::mat4(1.0f);
+	//mxModel = glm::scale(mxModel, glm::vec3(1.0f));
+	//glm::mat4 mx = mxProjection * mxView * mxModel;
+
+	transform.Update();
+	glm::mat4 mx = mxProjection * mxView * transform.matrix;
 	program.SetUniform("mx", mx);
 
 	bool quit = false;
@@ -84,14 +142,31 @@ int main(int argc, char** argv)
 
 		g_timer.tick();
 
-		float rotate = input->GetKey(SDL_SCANCODE_SPACE) ? 90.0f : 0.0f;
-		mx = glm::rotate(mx, glm::radians(rotate) * g_timer.dt(), glm::vec3(0.0f, 0.0f, 1.0f));
+		//float rotate = input->GetKey(SDL_SCANCODE_SPACE) ? 90.0f : 0.0f;
+		//mxModel = glm::rotate(mxModel, glm::radians(rotate) * g_timer.dt(), glm::vec3(0.0f, 0.0f, 1.0f));
+
+		glm::vec3 translate(0.0f, 0.0f, 0.0f);
+		float speed = 10.0f;
+		if (input->GetKey(SDL_SCANCODE_RIGHT))	translate.x =  speed;
+		if (input->GetKey(SDL_SCANCODE_LEFT))	translate.x = -speed;
+		if (input->GetKey(SDL_SCANCODE_UP))		translate.y =  speed;
+		if (input->GetKey(SDL_SCANCODE_DOWN))	translate.y = -speed;
+		if (input->GetKey(SDL_SCANCODE_W))		translate.z =  speed;
+		if (input->GetKey(SDL_SCANCODE_S))		translate.z = -speed;
+
+
+		//mxModel = glm::translate(mxModel, translate * g_timer.dt());
+		eye = eye + translate * g_timer.dt();
+		mxView = glm::lookAt(eye, glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+		//glm::mat4 mx = mxProjection * mxView * mxModel;
+		glm::mat4 mx = mxProjection * mxView * transform.matrix;
 		program.SetUniform("mx", mx);
 
 		renderer->ClearBuffer();
 
 		glBindVertexArray(vao);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(cube_elements) / sizeof(GLushort), GL_UNSIGNED_SHORT, 0);
 		glBindVertexArray(0);
 
 		renderer->SwapBuffer();
